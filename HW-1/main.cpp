784c6e7e188032f13eae71fa97af7f44e652fbd1
AlperMulayim <alpermulayim@gtu.edu.tr>
2017-03-11 16:41:00 +0300

    debug modes added

--- HW-1/main.cpp
+++ HW-1/main.cpp
@@ -2,6 +2,7 @@
 #include "8080emuCPP.h"
 #include "gtuos.h"
 
+using namespace std;
 int main (int argc, char**argv)
 {
 	if (argc != 3){
@@ -15,13 +16,31 @@ int main (int argc, char**argv)
 
 	theCPU.ReadFileIntoMemoryAt(argv[1], 0x0000);	
 
-	do	
-	{
-		theCPU.Emulate8080p(DEBUG);
-		if(theCPU.isSystemCall())
-			theOS.handleCall(theCPU);
-	}	while (!theCPU.isHalted())
-;
+    if(DEBUG == 0) {
+        do {
+            theCPU.Emulate8080p(DEBUG);
+            if (theCPU.isSystemCall())
+                theOS.handleCall(theCPU);
+        } while (!theCPU.isHalted());
+    }
+    else if(DEBUG == 1){
+        do {
+            theCPU.Emulate8080p(DEBUG);
+            if (theCPU.isSystemCall())
+                theOS.handleCall(theCPU);
+        } while (!theCPU.isHalted());
+
+    }else if(DEBUG == 2){
+        do { //press enter for debug mod
+            theCPU.Emulate8080p(DEBUG);
+            if (theCPU.isSystemCall()) {
+                theOS.handleCall(theCPU);
+                cin.get();
+            }
+            cin.get();
+        } while (!theCPU.isHalted());
+    }
+
 	return 0;
 }
 
